5315d5fce88f1d7664ec4329dffd222bc86ee19c
CedN <cnagat@gmail.com>
2021-03-14 00:37:38 +0100

    feat(domain): added word choosing

--- src/main/java/cna/apps/hangman/domain/Game.java
+++ src/main/java/cna/apps/hangman/domain/Game.java
@@ -2,4 +2,14 @@ package cna.apps.hangman.domain;
 
 public class Game {
 
+  private final WordProvider wordProvider;
+
+  public Game(WordProvider wordProvider) {
+    this.wordProvider = wordProvider;
+  }
+
+  public Word chooseWord() {
+    return new Word(wordProvider.provide());
+  }
+
 }
--- src/test/java/cna/apps/hangman/domain/GameShould.java
+++ src/test/java/cna/apps/hangman/domain/GameShould.java
@@ -1,7 +1,17 @@
 package cna.apps.hangman.domain;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import cna.apps.hangman.provider.WordList;
+
 public class GameShould {
   
-  
+  @Test
+  void choose_word() {
+    Game game = new Game(new WordList("hangman"));
+    assertNotNull(game.chooseWord());
+  }  
 
 }
